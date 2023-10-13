4a60c1fe54d3f634c6e094dd014965a4f200867c
Gregorio_Ramirez <iamgregor217@gmail.com>
2023-10-13 08:59:09 +0800

    Second change

--- app/src/main/java/com/example/capstone/LoginPage.kt
+++ app/src/main/java/com/example/capstone/LoginPage.kt
@@ -2,10 +2,21 @@ package com.example.capstone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        val loginbtn: Button = findViewById(R.id.LoginBtn)
+        val Uname: EditText = findViewById(R.id.Username)
+        val Pswrd: EditText = findViewById(R.id.Password)
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/capstone/SignUpPage.kt
+++ app/src/main/java/com/example/capstone/SignUpPage.kt
@@ -7,5 +7,9 @@ class SignUpPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_page)
+
+
+
+
     }
 }
