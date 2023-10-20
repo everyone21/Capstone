05fadcacc677e32286983e4cc8545f8e2bd22bfd
Gregorio_Ramirez <iamgregor217@gmail.com>
2023-10-20 11:20:43 +0800

    update

--- app/src/main/java/com/example/capstone/MainActivity.kt
+++ app/src/main/java/com/example/capstone/MainActivity.kt
@@ -2,26 +2,34 @@ package com.example.capstone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.capstone.databinding.ActivityMainBinding
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replace(Home())
-
+        currentFragment(Home())
         binding.bottomNavigationView.setOnItemSelectedListener {
 
             when(it.itemId){
-                R.id.home -> replace(Home())
-                R.id.calendar -> replace(Calendar())
-                R.id.upload -> replace(Report())
-                R.id.inbox -> replace(Inbox())
-                R.id.profile -> replace(Profile())
+                R.id.home -> currentFragment(Home())
+                R.id.calendar -> currentFragment(Calendar())
+                R.id.upload -> currentFragment(Report())
+                R.id.inbox -> currentFragment(Inbox())
+                R.id.profile -> currentFragment(Profile())
 
                 else ->{
 
@@ -30,15 +38,22 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+
+
     }
 
 
-    private fun replace(fragment: Fragment){
+    private fun currentFragment(fragment: Fragment){
 
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.container,fragment)
-        fragmentTransaction.commit()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container,fragment)
+            commit()
+
+
+        }
+//        val fragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.container,fragment)
+//        fragmentTransaction.commit()
 
     }
 
